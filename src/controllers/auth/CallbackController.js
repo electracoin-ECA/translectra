47e05c5862073536bf41fc53172167349e30f13e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-28 01:37:02 +0200

    controllers/auth/CallbackController: simplify

--- src/controllers/auth/CallbackController.js
+++ src/controllers/auth/CallbackController.js
@@ -5,24 +5,6 @@ import { User } from '../../models/User'
 
 export default class CallbackController extends BaseController {
   get() {
-    this.render('auth/login')
-
-    passport.authenticate('google', (err, user) => {
-      if (err !== null) {
-        this.answerError(err)
-
-        return
-      }
-
-      this.req.logIn(user, err => {
-        if (err !== null) {
-          this.answerError(err)
-
-          return
-        }
-      })
-
-      this.res.redirect('/')
-    })(this.req, this.res, this.next)
+    this.res.redirect('/')
   }
 }
