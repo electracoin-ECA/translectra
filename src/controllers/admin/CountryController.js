918bfcac4990d346baeab87e75197981b03eeaa6
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-28 17:40:05 +0200

    controllers/admin/CountryController: finish get() and add post() method

--- src/controllers/admin/CountryController.js
+++ src/controllers/admin/CountryController.js
@@ -1,7 +1,34 @@
 import BaseController from '..'
+import Country from '../../models/Country'
 
 export default class CountryController extends BaseController {
   get() {
-    this.render('admin/country')
+    Country.find((err, countries) => {
+      if (err !== null) {
+        this.answerError(err)
+
+        return
+      }
+
+      const errors = {}
+
+      this.render('admin/country', { countries })
+    })
+  }
+
+  post() {
+    const { name } = this.req.body
+    const nowDate = Date.now()
+
+    const country = new Country({
+      name,
+      createdAt: nowDate,
+      updatedAt: nowDate
+    })
+
+    country.save((err) => {
+      this.flashMongooseErrors(err, country)
+      this.res.redirect('/admin/country')
+    })
   }
 }
