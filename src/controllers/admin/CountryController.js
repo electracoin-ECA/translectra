0bfd764c3f9f7035ef112142b47dd3d1cbbc3090
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-30 19:10:26 +0200

    controllers/admin/CountryController: remove post() method

--- src/controllers/admin/CountryController.js
+++ src/controllers/admin/CountryController.js
@@ -15,20 +15,4 @@ export default class CountryController extends BaseController {
       this.render('admin/country', { countries })
     })
   }
-
-  post() {
-    const { name } = this.req.body
-    const nowDate = Date.now()
-
-    const country = new Country({
-      name,
-      createdAt: nowDate,
-      updatedAt: nowDate,
-    })
-
-    country.save((err) => {
-      this.flashMongooseErrors(err, country)
-      this.res.redirect('/admin/country')
-    })
-  }
 }
