534647c7d0d9278a45e362d8b6d85f49afeca15a
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-01 01:34:54 +0200

    controllers/admin/UserController: remove useless methods

--- src/controllers/admin/UserController.js
+++ src/controllers/admin/UserController.js
@@ -3,14 +3,6 @@ import User from '../../models/User'
 
 export default class UserController extends BaseController {
   get() {
-    User.find((err, users) => {
-      if (err !== null) {
-        this.answerError(err)
-
-        return
-      }
-
-      this.render('admin/user', { users })
-    })
+    this.render('admin/user')
   }
 }
