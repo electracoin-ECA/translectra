c984c524ed37fcf23f86356867039200c903deaf
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-01 01:34:42 +0200

    controllers/admin/CountryController: remove useless methods

--- src/controllers/admin/CountryController.js
+++ src/controllers/admin/CountryController.js
@@ -3,16 +3,6 @@ import Country from '../../models/Country'
 
 export default class CountryController extends BaseController {
   get() {
-    Country.find((err, countries) => {
-      if (err !== null) {
-        this.answerError(err)
-
-        return
-      }
-
-      const errors = {}
-
-      this.render('admin/country', { countries })
-    })
+    this.render('admin/country')
   }
 }
