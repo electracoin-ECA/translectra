9952ce0dec0672674e03103ad0418f0e41fed9f4
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-01 01:34:24 +0200

    controllers/api/UserController: create

---
+++ src/controllers/api/UserController.js
@@ -0,0 +1,71 @@
+import BaseController from '..'
+import User from '../../models/User'
+
+const HTTP_STATUS_CODE_OK = 200
+const HTTP_STATUS_CODE_CREATED = 201
+const HTTP_STATUS_CODE_ACCEPTED = 202
+const HTTP_STATUS_CODE_BAD_REQUEST = 400
+const HTTP_STATUS_CODE_NOT_FOUND = 404
+
+export default class UserController extends BaseController {
+  get() {
+    this.isJson = true
+
+    const sortData = {}
+    sortData[this.req.query.sortBy] = Number(this.req.query.sortOrder)
+
+    User
+      .find({
+        $or: [
+          { displayName: { $regex: new RegExp( this.req.query.query !== undefined ? this.req.query.query : '', 'i') } },
+          { email: { $regex: new RegExp( this.req.query.query !== undefined ? this.req.query.query : '', 'i') } },
+        ],
+      })
+      .sort(sortData)
+      .limit(this.req.query.limit !== undefined ? Number(this.req.query.limit) : 25)
+      .exec((err, countries) => {
+        if (err !== null) {
+          this.answerError(err)
+
+          return
+        }
+
+        this.res.status(HTTP_STATUS_CODE_OK).json(countries)
+      })
+  }
+
+  put() {
+    this.isJson = true
+
+    User.findByIdAndUpdate(
+      this.req.params.userId,
+      {
+        isAdmin: this.req.body.isAdmin,
+        isManager: this.req.body.isManager,
+        updatedAt: Date.now(),
+      },
+      (err) => {
+        if (err !== null) {
+          this.res.status(HTTP_STATUS_CODE_BAD_REQUEST).json(err.errors)
+
+          return
+        }
+
+        this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
+      })
+  }
+
+  delete() {
+    this.isJson = true
+
+    User.remove({ _id: this.req.params.userId }, err => {
+      if (err !== null) {
+        this.answerError(err)
+
+        return
+      }
+
+      this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
+    })
+  }
+}
