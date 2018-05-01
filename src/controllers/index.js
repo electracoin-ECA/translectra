688ee80a5173e4b82225e2969cc3864a21d29532
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-02 00:10:28 +0200

    controllers/index: remove release version

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,14 +4,12 @@ import * as lexpress from 'lexpress'
 dotenv.config()
 
 const VERSION = require('../../package.json').version
-const WEBSITE_NAME = require('../../package.json').version
 
 export default class BaseController extends lexpress.BaseController {
   render(view, data) {
     data = data || {}
     const global = {
       me: this.req.user,
-      releaseVersion: process.env[process.env.RELEASE_VERSION_ENV_VAR_NAME],
       version: VERSION,
       websiteName: process.env.WEBSITE_NAME,
     }
