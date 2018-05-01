e2c4883492ea21e9040bf2e43d8bf0fc66126dd7
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-02 01:30:59 +0200

    middlewares/isAdmin: allow admin access in dev env

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 export default function(req, res, next) {
-  if (req.user === undefined || !req.user.isActivated || !req.user.isAdmin) {
+  if (process.env.NODE_ENV !== 'development' && (req.user === undefined || !req.user.isActivated || !req.user.isAdmin)) {
     res.redirect('/')
 
     return
