3470c54b5830177b4cd8c2f59e14ef9eef6a6944
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-02 23:26:16 +0200

    controllers/management/KeyController: create

---
+++ src/controllers/management/KeyController.js
@@ -0,0 +1,31 @@
+import BaseController from '..'
+import Project from '../../models/Project'
+import User from '../../models/User'
+
+export default class KeyController extends BaseController {
+  get() {
+    User
+      .find()
+      .sort({ displayName: 1 })
+      .exec((err, users) => {
+        if (err !== null) {
+          this.answerError(err)
+
+          return
+        }
+
+        Project
+          .find()
+          .sort({ name: 1 })
+          .exec((err, projects) => {
+            if (err !== null) {
+              this.answerError(err)
+
+              return
+            }
+
+            this.render('management/key', { projects, users })
+          })
+      })
+  }
+}
