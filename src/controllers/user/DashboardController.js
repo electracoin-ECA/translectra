66e0258366fe3b881aaa42d7be90cdb78bfd1544
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 19:11:42 +0200

    controllers/user/DashboardController: create

---
+++ src/controllers/user/DashboardController.js
@@ -0,0 +1,31 @@
+import BaseController from '..'
+import Language from '../../models/Language'
+import Project from '../../models/Project'
+
+export default class DashboardController extends BaseController {
+  get() {
+    Project
+      .find()
+      .sort({ name: 1 })
+      .exec((err, projects) => {
+        if (err !== null) {
+          this.answerError(err)
+
+          return
+        }
+
+        Language
+          .find()
+          .sort({ name: 1 })
+          .exec((err, languages) => {
+            if (err !== null) {
+              this.answerError(err)
+
+              return
+            }
+
+            this.render('user/dashboard', { languages, projects })
+          })
+      })
+  }
+}
