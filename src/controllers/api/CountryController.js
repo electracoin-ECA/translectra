5e4086d259bdb80bce2655e4cd27b9e5582f7e4c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 17:46:18 +0200

    controllers/api/CountryController: dry

--- src/controllers/api/CountryController.js
+++ src/controllers/api/CountryController.js
@@ -1,95 +1,20 @@
 import BaseController from '..'
 import Country from '../../models/Country'
 
-const HTTP_STATUS_CODE_OK = 200
-const HTTP_STATUS_CODE_CREATED = 201
-const HTTP_STATUS_CODE_ACCEPTED = 202
-const HTTP_STATUS_CODE_BAD_REQUEST = 400
-const HTTP_STATUS_CODE_NOT_FOUND = 404
-const LIMIT_DEFAULT = 25
-const LIMIT_MAX = 100
-
 export default class CountryController extends BaseController {
   get() {
-    this.isJson = true
-
-    const limit = this.req.query.limit !== undefined ? Number(this.req.query.limit) : LIMIT_DEFAULT
-    const sortData = {}
-    sortData[this.req.query.sortBy] = Number(this.req.query.sortOrder)
-
-    Country
-      .find({
-        $or: [
-          { name:{ $regex: new RegExp( this.req.query.query !== undefined ? this.req.query.query : '', 'i') } },
-        ],
-      })
-      .sort(sortData)
-      .limit(limit > LIMIT_MAX ? LIMIT_MAX : limit)
-      .exec((err, countries) => {
-        if (err !== null) {
-          this.answerError(err)
-
-          return
-        }
-
-        this.res.status(HTTP_STATUS_CODE_OK).json(countries)
-      })
+    this.apiGet(Country, ['name'])
   }
 
   post() {
-    this.isJson = true
-
-    const { name } = this.req.body
-    const nowDate = Date.now()
-
-    const country = new Country({
-      name,
-      createdAt: nowDate,
-      updatedAt: nowDate,
-    })
-
-    country.save((err) => {
-      if (err !== null) {
-        this.res.status(HTTP_STATUS_CODE_BAD_REQUEST).json(err.errors)
-
-        return
-      }
-
-      this.res.status(HTTP_STATUS_CODE_CREATED).json({})
-    })
+    this.apiPost(Country, ['name'])
   }
 
   put() {
-    this.isJson = true
-
-    Country.findByIdAndUpdate(
-      this.req.params.countryId,
-      {
-        name: this.req.body.name,
-        updatedAt: Date.now(),
-      },
-      (err) => {
-        if (err !== null) {
-          this.res.status(HTTP_STATUS_CODE_BAD_REQUEST).json(err.errors)
-
-          return
-        }
-
-        this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
-      })
+    this.apiPut(Country, ['name'])
   }
 
   delete() {
-    this.isJson = true
-
-    Country.remove({ _id: this.req.params.countryId }, err => {
-      if (err !== null) {
-        this.answerError(err)
-
-        return
-      }
-
-      this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
-    })
+    this.apiDelete(Country)
   }
 }
