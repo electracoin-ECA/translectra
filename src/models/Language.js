36099d9b106639535922d87176c309dae1ff8d10
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 16:27:52 +0200

    models/Language: add validation messages

--- src/models/Language.js
+++ src/models/Language.js
@@ -7,16 +7,17 @@ const languageSchema = new mongoose.Schema({
   country: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Country',
+    required: [true, 'The language country is a required field.'],
   },
   name: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, 'The language name is a required field.'],
+    unique: [true, 'This language name already exists.'],
   },
   code: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, 'The language code is a required field.'],
+    unique: [true, 'This language code already exists.'],
     validate: {
       validator: v => v.length === 0 || /^[a-z]{2}-[A-Z]{2}$/.test(v),
       message: `The language code must be of form "xx-XX".`,
