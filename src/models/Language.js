01ce7c17693cc62450b8dbad7c00f45f131c2170
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 19:10:51 +0200

    models/Language: fix validation messages

--- src/models/Language.js
+++ src/models/Language.js
@@ -7,17 +7,17 @@ const languageSchema = new mongoose.Schema({
   country: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Country',
-    required: [true, 'The language country is a required field.'],
+    required: [true, `The language country is a required field.`],
   },
   name: {
     type: String,
-    required: [true, 'The language name is a required field.'],
-    unique: [true, 'This language name already exists.'],
+    required: [true, `The language name is a required field.`],
+    unique: true,
   },
   code: {
     type: String,
-    required: [true, 'The language code is a required field.'],
-    unique: [true, 'This language code already exists.'],
+    required: [true, `The language code is a required field.`],
+    unique: true,
     validate: {
       validator: v => v.length === 0 || /^[a-z]{2}-[A-Z]{2}$/.test(v),
       message: `The language code must be of form "xx-XX".`,
@@ -33,6 +33,6 @@ const languageSchema = new mongoose.Schema({
   },
 })
 
-languageSchema.plugin(mongooseUniqueValidator)
+languageSchema.plugin(mongooseUniqueValidator, { message: `This language {PATH} already exists.` })
 
 export default mongoose.model('Language', languageSchema)
