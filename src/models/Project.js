6ef6dafb62c432effc1b93670283dc2dbbe2dbaa
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 16:28:28 +0200

    models/Project: add versions children

--- src/models/Project.js
+++ src/models/Project.js
@@ -4,9 +4,17 @@ import mongooseUniqueValidator from 'mongoose-unique-validator'
 const projectSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, `The project name is a required field.`],
+    unique: [true, `This project name is already taken.`],
   },
+  versions: [{
+    type: String,
+    unique: [true, `You can't have duplicates in your project version names.`],
+    validate: {
+      validator: v => v.length === 0 || /^v\d+\.\d+.\d+$/.test(v),
+      message: `The project versions must be of form "vX.Y.Z" (X, Y & Z being numbers).`,
+    },
+  }],
   createdAt: {
     type: Date,
     required: true,
