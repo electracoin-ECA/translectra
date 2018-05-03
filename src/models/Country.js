72bc379a7b52231c70d9e260f4bbf62a9da86366
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-03 16:26:17 +0200

    models/Country: add validation messages

--- src/models/Country.js
+++ src/models/Country.js
@@ -4,8 +4,8 @@ import mongooseUniqueValidator from 'mongoose-unique-validator'
 const countrySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: [true, 'This country name is already taken.'],
+    required: [true, 'The country name is a required field.'],
+    unique: [true, 'This country name already exists.'],
   },
   createdAt: {
     type: Date,
