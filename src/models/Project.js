05c3d71ce8cf9119ed07d76a56be310afad0dc77
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-04 16:43:42 +0200

    models/Project: integrate Version model

--- src/models/Project.js
+++ src/models/Project.js
@@ -2,13 +2,18 @@ import mongoose from 'mongoose'
 import mongooseUniqueValidator from 'mongoose-unique-validator'
 import R from 'ramda'
 
+import Version from './Version'
+
 const projectSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, `The project name is a required field.`],
     unique: [true, `This project name is already taken.`],
   },
-  versions: [String],
+  versions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Version',
+  }],
   createdAt: {
     type: Date,
     required: true,
@@ -23,14 +28,6 @@ projectSchema.plugin(mongooseUniqueValidator)
 projectSchema.path('versions').validate(
   versions => Array.isArray(versions) && versions.length > 0,
   `You must create at least one version for each project.`
-)
-projectSchema.path('versions').validate(
-  versions => !Boolean(versions.filter(version => !/^v\d+\.\d+.\d+$/.test(version)).length),
-  `The project versions must be of form "vX.Y.Z" (X, Y & Z being numbers).`
-)
-projectSchema.path('versions').validate(
-  versions => versions.length === R.uniq(versions).length,
-  `You can't have duplicates in your project versions.`
 )
 
 export default mongoose.model('Project', projectSchema)
