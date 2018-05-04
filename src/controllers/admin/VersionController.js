79d61539ceae32fc3066e9e5f4955e22932ce24f
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-04 16:41:48 +0200

    controllers/admin/VersionController: create

---
+++ src/controllers/admin/VersionController.js
@@ -0,0 +1,19 @@
+import BaseController from '..'
+import Project from '../../models/Project'
+
+export default class VersionController extends BaseController {
+  get() {
+    Project
+      .find()
+      .sort({ name: 1 })
+      .exec((err, projects) => {
+        if (err !== null) {
+          this.answerError(err)
+
+          return
+        }
+
+        this.render('admin/language', { projects })
+      })
+  }
+}
