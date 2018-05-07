9fc96a2cf4fa782e2649d1f1c6a36fc592ad137f
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-07 07:36:55 +0200

    controllers/admin/ProjectController: add missing users

--- src/controllers/admin/ProjectController.js
+++ src/controllers/admin/ProjectController.js
@@ -1,7 +1,19 @@
 import BaseController from '..'
+import User from '../../models/User'
 
 export default class ProjectController extends BaseController {
   get() {
-    this.render('admin/projects')
+    User
+      .find()
+      .sort({ displayName: 1 })
+      .exec((err, users) => {
+        if (err !== null) {
+          this.answerError(err)
+
+          return
+        }
+
+        this.render('admin/projects', { users })
+      })
   }
 }
