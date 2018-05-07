805c396620870e62cec885926a31d373817299ad
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-07 08:27:46 +0200

    controllers/api/KeyController: simplify and fix delete() method

--- src/controllers/api/KeyController.js
+++ src/controllers/api/KeyController.js
@@ -1,107 +1,27 @@
 import BaseController from '..'
 import Key from '../../models/Key'
-import Project from '../../models/Project'
 
-const HTTP_STATUS_CODE_OK = 200
-const HTTP_STATUS_CODE_CREATED = 201
-const HTTP_STATUS_CODE_ACCEPTED = 202
-const HTTP_STATUS_CODE_BAD_REQUEST = 400
-const HTTP_STATUS_CODE_NOT_FOUND = 404
-const LIMIT_DEFAULT = 25
-const LIMIT_MAX = 100
+import { HTTP_STATUS_CODE_ACCEPTED } from '..'
 
 export default class KeyController extends BaseController {
   get() {
-    this.isJson = true
-
-    const limit = this.req.query.limit !== undefined ? Number(this.req.query.limit) : LIMIT_DEFAULT
-    const sortData = {}
-    sortData[this.req.query.sortBy] = Number(this.req.query.sortOrder)
-
-    Key
-      .find({
-        $or: [
-          { name: { $regex: new RegExp( this.req.query.query !== undefined ? this.req.query.query : '', 'i') } },
-          { value: { $regex: new RegExp( this.req.query.query !== undefined ? this.req.query.query : '', 'i') } },
-        ],
-      })
-      .sort(sortData)
-      .limit(limit > LIMIT_MAX ? LIMIT_MAX : limit)
-      .populate('author')
-      .populate('projects')
-      .exec((err, keys) => {
-        if (err !== null) {
-          this.answerError(err)
-
-          return
-        }
-
-        this.res.status(HTTP_STATUS_CODE_OK).json(keys)
-      })
+    this.apiGet(Key, ['name', 'value'], ['projects', 'author'])
   }
 
   post() {
-    this.isJson = true
-
-    const key = new Key({
-      name: this.req.body.name,
-      projects: this.req.body.projects,
-      note: this.req.body.note,
-      url: this.req.body.url,
-      value: this.req.body.value,
-      author: this.req.user._id,
-      translations: [],
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    })
-
-    key.save((err) => {
-      if (err !== null) {
-        this.res.status(HTTP_STATUS_CODE_BAD_REQUEST).json(err.errors)
-
-        return
-      }
-
-      this.res.status(HTTP_STATUS_CODE_CREATED).json({})
-    })
+    this.req.body.author = this.req.user._id
+    this.apiPost(Key, ['name', 'projects', 'note', 'url', 'value', 'author'])
   }
 
   put() {
-    this.isJson = true
-
-    Key.findByIdAndUpdate(
-      this.req.params.keyId,
-      {
-        name: this.req.body.name,
-        projects: this.req.body.projects,
-        note: this.req.body.note,
-        url: this.req.body.url,
-        value: this.req.body.value,
-        updatedAt: Date.now(),
-      },
-      (err) => {
-        if (err !== null) {
-          this.res.status(HTTP_STATUS_CODE_BAD_REQUEST).json(err.errors)
-
-          return
-        }
-
-        this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
-      }
-    )
+    this.apiPut(Key, ['name', 'projects', 'note', 'url', 'value'])
   }
 
   delete() {
     this.isJson = true
 
-    Key.remove({ _id: this.req.params.keyId }, err => {
-      if (err !== null) {
-        this.answerError(err)
-
-        return
-      }
-
-      this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({})
-    })
+    this.removeKeys(this.req.params.id)
+      .then(() => this.res.status(HTTP_STATUS_CODE_ACCEPTED).json({}))
+      .catch(this.answerError.bind(this))
   }
 }
