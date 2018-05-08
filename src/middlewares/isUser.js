1908508805287176997be26364c1dae90b8f766e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 13:42:29 +0200

    middlewares/isUser: remove dev condition

--- src/middlewares/isUser.js
+++ src/middlewares/isUser.js
@@ -1,5 +1,5 @@
 export default function(req, res, next) {
-  if (process.env.NODE_ENV !== 'development' && (req.user === undefined || !req.user.isActivated)) {
+  if (req.user === undefined || !req.user.isActivated) {
     res.redirect('/')
 
     return
