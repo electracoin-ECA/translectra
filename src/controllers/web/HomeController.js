f3153b5abdfeb2c9c55fd6613168e3370202a812
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 22:11:22 +0200

    controllers/web/HomeController: add real stats

--- src/controllers/web/HomeController.js
+++ src/controllers/web/HomeController.js
@@ -1,31 +1,45 @@
 import BaseController from '..'
+import KeyLanguage from '../../models/KeyLanguage'
 import Language from '../../models/Language'
 import Project from '../../models/Project'
 
 export default class HomeController extends BaseController {
   get() {
-    Project
-      .find()
-      .sort({ name: 1 })
-      .exec((err, projects) => {
-        if (err !== null) {
-          this.answerError(err)
+    Promise.all([
+      this.findWhere(Project, {}, 'name'),
+      this.findWhere(Language, {}, 'name'),
+      this.findWhere(KeyLanguage, {}),
+    ])
+      .then(([_projects, _languages, _keysLanguages]) => {
+        const languages = _languages.map(_language => ({
+          id: _language.id,
+          name: _language.name,
+          count: _keysLanguages
+            .filter(_keyLanguage => String(_keyLanguage.language) === _language.id)
+            .length,
+          doneCount: _keysLanguages
+            .filter(_keyLanguage => String(_keyLanguage.language) === _language.id && _keyLanguage.isDone)
+            .length,
+        }))
 
-          return
-        }
+        const projects = _projects.map(_project => ({
+          id: _project.id,
+          name: _project.name,
+          count: _keysLanguages
+            .filter(_keyLanguage =>
+              _keyLanguage.projects.map(_projectId => String(_projectId)).includes(_project.id)
+            )
+            .length,
+          doneCount: _keysLanguages
+            .filter(_keyLanguage =>
+              _keyLanguage.projects.map(_projectId => String(_projectId)).includes(_project.id) &&
+              _keyLanguage.isDone
+            )
+            .length,
+        }))
 
-        Language
-          .find()
-          .sort({ name: 1 })
-          .exec((err, languages) => {
-            if (err !== null) {
-              this.answerError(err)
-
-              return
-            }
-
-            this.render('web/home', { languages, projects })
-          })
+        this.render('web/home', { languages, projects })
       })
+      .catch(this.answerError.bind(this))
   }
 }
