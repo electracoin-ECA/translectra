eda82e006b3064bee499627c329a587bc52019a1
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 21:12:39 +0200

    app/translate-table: finish first version of translations management

--- app/translate-table/index.jsx
+++ app/translate-table/index.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Table from './table'
 
 const API_URL = '/api/translate'
-const FETCH_INTERVAL = 10000
+const FETCH_INTERVAL = 5000
 
 export default class App extends React.PureComponent {
   constructor(props) {
@@ -23,12 +23,29 @@ export default class App extends React.PureComponent {
     }
   }
 
-  componentDidMount() {
-    this.fetch(true)
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.currentLanguageId !== prevState.currentLanguageId ||
+      this.state.currentProjectId !== prevState.currentProjectId ||
+      this.state.currentStatus !== prevState.currentStatus
+    ) {
+      if (this.fetchTimeoutId !== undefined) {
+        clearInterval(this.fetchTimeoutId)
+        delete this.fetchTimeoutId
+      }
+
+      this.fetch(true)
+    }
   }
 
   fetch(mustLoop) {
-    const params = {}
+    if (this.state.currentLanguageId.length === 0) return
+
+    const params = {
+      languageId: this.state.currentLanguageId,
+      projectId: this.state.currentProjectId,
+      isDone: Number(this.state.currentStatus === 'DONE'),
+    }
     if (this.$searchInput.value.length !== 0) params.query = this.$searchInput.value
 
     axios.get(API_URL, { params })
@@ -37,16 +54,73 @@ export default class App extends React.PureComponent {
           isLoading: !mustLoop && this.state.isLoading ? false : this.state.isLoading,
           items: data,
         })
-        if (mustLoop) setTimeout(() => this.fetch(true), FETCH_INTERVAL)
+        if (mustLoop && this.state.currentLanguageId.length !== 0) {
+          this.fetchTimeoutId = setTimeout(() => this.fetch(true), FETCH_INTERVAL)
+        }
       })
+  }
+
+  create(keyLanguageId, value) {
+    if (this.state.isLoading) return
+
+    this.setState({ isLoading: true })
+    const data = {
+      keyLanguageId,
+      language: this.state.currentLanguageId,
+      value,
+    }
+
+    axios.post(`${API_URL}`, data)
+      .then(() => {
+        this.setState({
+          formData: undefined,
+          formErrors: {},
+          formKey: this.state.formKey + 1,
+        })
+        this.fetch(false)
+      })
+      .catch(({ data }) => {
+        this.setState({
+          formErrors: data,
+          isLoading: false,
+        })
+      })
+  }
+
+  accept(keyLanguageId, translationId, isAccepted) {
+    if (this.state.isLoading) return
+
+    this.setState({ isLoading: true })
+    const data = {
+      isAccepted,
+      keyLanguageId,
+    }
+
+    axios.put(`${API_URL}/${translationId}/accept`, data)
+      .then(() => this.fetch(false))
+      .catch(() => this.setState({ isLoading: false }))
+  }
+
+  vote(translationId, state) {
+    if (this.state.isLoading) return
+
+    this.setState({ isLoading: true })
+
+    axios.put(`${API_URL}/${translationId}/${state === 1 ? 'up' : 'down'}Vote`)
+      .then(() => this.fetch(false))
+      .catch(() => this.setState({ isLoading: false }))
+  }
+
+  selectLanguage() {
+    this.setState({ currentLanguageId: this.$languageSelect.value })
   }
 
   selectProject() {
     this.setState({ currentProjectId: this.$projectSelect.value })
   }
 
-  selectLanguage() {
-    this.setState({ currentLanguageId: this.$languageSelect.value })
+  selectStatus() {
+    this.setState({ currentStatus: this.$statusSelect.value })
   }
 
   render() {
@@ -69,42 +143,31 @@ export default class App extends React.PureComponent {
           </div>
         </div>
 
-        {/* {this.state.isFormOpen && (
-          <Form
-            action={this.state.formAction}
-            errors={this.state.formErrors}
-            foreignData={this.props.meta.foreignData}
-            initialData={this.state.formData}
-            isLoading={this.state.isLoading}
-            key={this.state.formKey}
-            onSubmit={this.state.formAction === 'create' ? this.create.bind(this) : this.update.bind(this)}
-            schema={this.props.schema.filter(({ isField }) => isField)}
-          />
-        )} */}
-
         <div className='d-flex justify-content-end form-inline mb-4'>
           <select
             className='form-control mr-2'
             defaultValue='PENDING'
-            onChange={this.selectProject.bind(this)}
-            ref={node => this.$projectSelect = node}
+            disabled={this.state.isLoading}
+            onChange={this.selectStatus.bind(this)}
+            ref={node => this.$statusSelect = node}
           >
-            <option children='Status:' key={'all'} value='' />
             <option children={'Pending'} key={'pending'} value={'PENDING'} />
             <option children={'Done'} key={'done'} value={'DONE'} />
           </select>
           <select
             className='form-control mr-2'
+            disabled={this.state.isLoading}
             onChange={this.selectProject.bind(this)}
             ref={node => this.$projectSelect = node}
           >
-            <option children='Project:' key={'all'} value='' />
+            <option children='All Projects' key={'all'} value='' />
             {this.props.meta.foreignData.projects.map(({ _id, name }) =>
               <option children={name} key={_id} value={_id} />
             )}
           </select>
           <select
             className='form-control'
+            disabled={this.state.isLoading}
             onChange={this.selectLanguage.bind(this)}
             ref={node => this.$languageSelect = node}
           >
@@ -126,7 +189,18 @@ export default class App extends React.PureComponent {
               />
             </div>
           )
-          : <Table isLoading={this.state.isLoading} items={this.state.items} />
+          : (
+            <Table
+              errors={this.state.formErrors}
+              onAccept={this.accept.bind(this)}
+              onSubmit={this.create.bind(this)}
+              onVote={this.vote.bind(this)}
+              isLoading={this.state.isLoading}
+              isManager={this.props.isManager}
+              items={this.state.items}
+              userId={this.props.userId}
+            />
+          )
         }
       </div>
     )
--- app/translate-table/table.jsx
+++ app/translate-table/table.jsx
@@ -1,59 +1,192 @@
 import moment from 'moment'
+import * as R from 'ramda'
 import React from 'react'
 
+import capitalizeFirstLetter from '../helpers/capitalizeFirstLetter'
+
 export default class Table extends React.PureComponent {
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
 
-  //   this.state = {
-  //     openedKeyId: '',
-  //   }
-  // }
+    this.state = {
+      openedKeyLanguageId: '',
+    }
+  }
 
-  renderRow(key) {
+  submit(event) {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.openedKeyLanguageId, this.$translationValueTextarea.value)
+  }
+
+  toggleTranslationForm(keyId) {
+    this.setState({
+      openedKeyLanguageId: this.state.openedKeyLanguageId === keyId ? '' : keyId,
+    })
+  }
+
+  renderForm(keyId) {
+    const hasError = this.props.errors.value !== undefined
+
+    return (
+      <tr key={`${keyId}-form`}>
+        <td className='border-top-0 p-0' colSpan='6'>
+          <form
+            autoComplete='off'
+            className='form bg-light p-2 border mb-2'
+            noValidate
+            onSubmit={this.submit.bind(this)}
+          >
+            <div className='form-group'>
+              <textarea
+                autoCapitalize='off'
+                autoCorrect='off'
+                className={['form-control', hasError ? 'is-invalid' : ''].join(' ').trim()}
+                disabled={this.props.isLoading}
+                ref={node => this.$translationValueTextarea = node}
+                spellCheck='false'
+              />
+              <div className='invalid-feedback'>{hasError && this.props.errors.value.message}</div>
+            </div>
+            <button
+              children='Submit'
+              className='btn btn-sm btn-primary'
+              disabled={this.props.isLoading}
+              type='submit'
+            />
+          </form>
+        </td>
+      </tr>
+    )
+  }
+
+  renderTranslations(keyId, translations) {
+    const translationsWithScore = translations.map(translation => ({
+      ...translation,
+      score: translation.upVotes.length - translation.downVotes.length,
+    }))
+
+    const translationsWithScoreSorted = R.sortWith([
+      R.descend(R.prop('isAccepted')),
+      R.descend(R.prop('score')),
+    ])(translationsWithScore)
+
+    return (
+      <tr key={`${keyId}-translations`}>
+        <td className='border-top-0 p-2' colSpan='6'>
+          {translationsWithScoreSorted.map(translation => {
+            const voteUpActionClass = translation.author._id !== this.props.userId
+              ? translation.upVotes.includes(this.props.userId) ? 'translation__action-on' : 'translation__action-off'
+              : 'translation__icon text-secondary'
+            const voteDownActionClass = translation.author._id !== this.props.userId
+              ? translation.downVotes.includes(this.props.userId) ? 'translation__action-on' : 'translation__action-off'
+              : 'translation__icon text-secondary'
+            const acceptActionClass = this.props.isManager
+              ? translation.isAccepted ? 'translation__action-on' : 'translation__action-off'
+              : translation.isAccepted ? 'translation__icon text-success' : 'translation__icon text-light'
+
+            return (
+              <div className='mb-3' key={translations._id}>
+                <div className='d-flex justify-content-between bg-light border p-1'>
+                  <div className=''>{translation.value}</div>
+                  <div className='d-flex flex-column align-items-center pr-0 no-select' style={{ minWidth: '2rem' }}>
+                    <i
+                      children='arrow_drop_up'
+                      className={`material-icons ${voteUpActionClass}`}
+                      onClick={() => translation.author._id !== this.props.userId
+                        ? this.props.onVote(translation._id, 1)
+                        : void 0
+                      }
+                    />
+                    <div>{translation.score}</div>
+                    <i
+                      children='arrow_drop_down'
+                      className={`material-icons ${voteDownActionClass}`}
+                      onClick={() => translation.author._id !== this.props.userId
+                        ? this.props.onVote(translation._id, -1)
+                        : void 0
+                      }
+                    />
+                    <i
+                      children='check'
+                      className={`material-icons ${acceptActionClass}`}
+                      onClick={() => this.props.isManager
+                        ? this.props.onAccept(keyId, translation._id, !translation.isAccepted)
+                        : void 0
+                      }
+                    />
+                  </div>
+                </div>
+                <div className='mt-1 text-secondary no-select' style={{ fontSize: '12px' }}>
+                  {`${capitalizeFirstLetter(moment(translation.createdAt).fromNow())} by ${translation.author.name}.`}
+                </div>
+              </div>
+            )
+          })}
+        </td>
+      </tr>
+    )
+  }
+
+  renderRow(keyLanguage) {
     const buttonClass = this.props.isLoading ? 'text-muted' : 'text-primary'
 
     return [
-      <tr key={key._id}>
-        <td key={`${key._id}-name`}>{key.name}</td>
-        <td key={`${key._id}-updatedAt`}>{moment(key.updatedAt).fromNow()}</td>
-        {key.note !== undefined && key.note.length !== 0
+      <tr className='text-secondary' key={keyLanguage._id} style={{ fontSize: '12px' }}>
+        <td key={`${keyLanguage._id}-name`}>{keyLanguage.key.name}</td>
+        <td key={`${keyLanguage._id}-version`}>v{keyLanguage.version}</td>
+        <td key={`${keyLanguage._id}-updatedAt`}>{moment(keyLanguage.createdAt).fromNow()}</td>
+        {keyLanguage.note !== undefined && keyLanguage.note.length !== 0
           ? (
             <td
               className='text-center text-info list__iconCell list__iconCell-action'
-              key={`${key._id}-note`}
-              title={key.note}
+              key={`${keyLanguage._id}-note`}
+              title={keyLanguage.key.note}
             >
               <i children='note' className='material-icons' />
             </td>
           )
-          : <td className='list__iconCell' key={`${key._id}-note`} />
+          : <td className='list__iconCell' key={`${keyLanguage._id}-note`} />
         }
-        {key.url !== undefined && key.url.length !== 0
+        {keyLanguage.key.url !== undefined && keyLanguage.key.url.length !== 0
           ? (
             <td
               className='text-center text-info list__iconCell list__iconCell-action'
-              key={`${key._id}-url`}
-              onClick={() => window.open(key.url, '_blank')}
+              key={`${keyLanguage._id}-url`}
+              onClick={() => window.open(keyLanguage.key.url, '_blank')}
             >
               <i className='material-icons'>link</i>
             </td>
           )
-          : <td className='list__iconCell' key={`${key._id}-url`} />
+          : <td className='list__iconCell' key={`${keyLanguage._id}-url`} />
         }
+        <td
+          className='text-right text-info'
+          key={`${keyLanguage._id}-translate`}
+          rowSpan='2'
+        >
+          <button
+            children='Translate'
+            className='btn btn-sm btn-success'
+            onClick={() => this.toggleTranslationForm(keyLanguage._id)}
+            type='button'
+          />
+        </td>
       </tr>,
-      <tr key={`${key._id}-projects`}>
-        <td className='pt-0 border-top-0' colSpan='4' style={{ lineHeight: 1 }}>
-          {key.projects.map(({ _id, name: _name }) =>
+      <tr key={`${keyLanguage._id}-projects`}>
+        <td className='pt-0 border-top-0' colSpan='5' style={{ lineHeight: 1 }}>
+          {keyLanguage.projects.map(({ _id, name: _name }) =>
             <span children={_name} className='badge badge-info no-select mr-1' key={_id} />,
           )}
         </td>
       </tr>,
-      <tr key={`${key._id}-value`}>
-        <td className='bg-light font-italic pt-0 border-top-0' colSpan='4'>
-          {key.value}
+      <tr key={`${keyLanguage._id}-value`}>
+        <td className='font-weight-bold pt-0 border-top-0' colSpan='6'>
+          {keyLanguage.key.value}
         </td>
-      </tr>
+      </tr>,
+      keyLanguage._id === this.state.openedKeyLanguageId && this.renderForm(keyLanguage._id),
+      keyLanguage.translations.length !== 0 && this.renderTranslations(keyLanguage._id, keyLanguage.translations),
     ]
   }
 
@@ -63,12 +196,14 @@ export default class Table extends React.PureComponent {
         <thead>
           <tr className={`no-select${this.props.isLoading ? ' text-muted' : ''}`}>
             <th key='name' scope='col'>Name</th>
+            <th key='version' scope='col'>Version</th>
             <th key={'updatedAt'} scope='col'>
-              Updated
+              Created
               <i className='material-icons'>arrow_drop_down</i>
             </th>
-            <th className='text-center' key='note' scope='col' />
-            <th className='text-center' key='url' scope='col' />
+            <th key='note' scope='col' />
+            <th key='url' scope='col' />
+            <th key='translate' scope='col' />
           </tr>
         </thead>
         <tbody>
