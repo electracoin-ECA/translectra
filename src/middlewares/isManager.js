a37c1743453995789ee7124c35ef279753955558
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 13:42:33 +0200

    middlewares/isManager: remove dev condition

--- src/middlewares/isManager.js
+++ src/middlewares/isManager.js
@@ -1,5 +1,5 @@
 export default function(req, res, next) {
-  if (process.env.NODE_ENV !== 'development' && (req.user === undefined || !req.user.isActivated || !req.user.isManager)) {
+  if (req.user === undefined || !req.user.isActivated || !req.user.isManager) {
     res.redirect('/')
 
     return
