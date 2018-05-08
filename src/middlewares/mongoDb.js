51213a8c40983a768df58ab60d7198b3d5e5d370
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 18:38:13 +0200

    middlewares/mongoDb: avoid re-connection when already connected

--- src/middlewares/mongoDb.js
+++ src/middlewares/mongoDb.js
@@ -2,6 +2,12 @@ import log from '@inspired-beings/log'
 import mongoose from 'mongoose'
 
 export default function(req, res, next) {
+  if (mongoose.connection.readyState !== 0) {
+    next()
+
+    return
+  }
+
   mongoose.connect(process.env.MONGODB_URI)
     .then(() => next())
     .catch(log.err)
