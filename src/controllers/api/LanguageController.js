299ce61257a8ea2778f17297587b7d55fb8bef21
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 23:05:14 +0200

    controllers/api/LanguageController: handle pivot table

--- src/controllers/api/LanguageController.js
+++ src/controllers/api/LanguageController.js
@@ -1,5 +1,17 @@
 import BaseController from '..'
+import Key from '../../models/Key'
+import KeyLanguage from '../../models/KeyLanguage'
 import Language from '../../models/Language'
+import Translation from '../../models/Translation'
+
+import {
+  HTTP_STATUS_CODE_ACCEPTED,
+  HTTP_STATUS_CODE_OK,
+  HTTP_STATUS_CODE_BAD_REQUEST,
+  HTTP_STATUS_CODE_CREATED,
+  HTTP_STATUS_CODE_FORBIDDEN,
+  HTTP_STATUS_CODE_NOT_FOUND,
+} from '..'
 
 export default class LanguageController extends BaseController {
   get() {
@@ -7,14 +19,52 @@ export default class LanguageController extends BaseController {
   }
 
   post() {
-    this.apiPost(Language, ['country', 'name', 'code'])
+    this.isJson = true
+
+    this.create(Language, {
+      name: this.req.body.name,
+      code: this.req.body.code,
+    })
+      .then(language => {
+        Key.find((err, keys) => {
+          if (err !== null) {
+            this.answerError(err)
+
+            return
+          }
+
+          Promise.all(keys.map(key => this.create(KeyLanguage, {
+            key: key.id,
+            version: key.version,
+            language: language.id,
+            projects: key.projects,
+          })))
+            .then(() => this.res.status(HTTP_STATUS_CODE_CREATED).json({}))
+            .catch(this.answerError.bind(this))
+        })
+      })
   }
 
   put() {
-    this.apiPut(Language, ['country', 'name', 'code'])
+    this.apiPut(Language, ['name', 'code'])
   }
 
   delete() {
-    this.apiDelete(Language)
+    KeyLanguage.find({ language: this.req.params.id }, (err, keysLanguages) => {
+      if (err !== null) {
+        this.answerError(err)
+
+        return
+      }
+
+      const translationIds = keysLanguages.reduce((prev, { translations }) => [...prev, ...translations], [])
+
+      Promise.all([
+        this.removeWhere(KeyLanguage, { language: this.req.params.id }),
+        this.remove(Translation, translationIds),
+      ])
+        .then(() => this.apiDelete(Language))
+        .catch(this.answerError.bind(this))
+    })
   }
 }
