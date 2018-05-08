002cdc55ce3aa8dc29fbff4e6d1880e3d9cc7dcf
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-08 13:42:38 +0200

    middlewares/isAdmin: remove dev condition

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 export default function(req, res, next) {
-  if (process.env.NODE_ENV !== 'development' && (req.user === undefined || !req.user.isActivated || !req.user.isAdmin)) {
+  if (req.user === undefined || !req.user.isActivated || !req.user.isAdmin) {
     res.redirect('/')
 
     return
