7eb990c635daec77898ba7ca822da2a63c3cf68e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-05-10 14:59:54 +0200

    app/index: integrate KeysTable

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import DataTable from './data-table'
+import KeysTable from './keys-table'
 import TranslateTable from './translate-table'
 
 import getMeta from './helpers/getMeta'
@@ -10,6 +11,13 @@ if (document.getElementById('data-table') !== null) {
   ReactDOM.render(
     <DataTable meta={getMeta('meta')} model={getMeta('model')} schema={getMeta('schema')} />,
     document.getElementById('data-table')
+  )
+}
+
+if (document.getElementById('keys-table') !== null) {
+  ReactDOM.render(
+    <KeysTable meta={getMeta('meta')} model={getMeta('model')} schema={getMeta('schema')} />,
+    document.getElementById('keys-table')
   )
 }
 
